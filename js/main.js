dc8f5bd0496f2d65843aa6a695b0fa0aad5d5c69
Кексель Наина Сергеевна <you@example.com>
2025-02-07 15:14:18 +0700

    six

--- js/main.js
+++ js/main.js
@@ -1,116 +1,3 @@
-Vue.component('product', {
-    template: `
-       <div class="product">
-            <div class="product-image">
-                <img v-bind:alt="altText" v-bind:src="image">
-            </div>
-            <div class="product-info">
-                <h1>{{ title }}</h1>
-                <p>{{description}}</p>
-                <span >{{ sale }}</span>
-                <p v-if="inStock" >В наличии</p>
-                <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
-                <p>Доставка: {{ shipping }}</p>
-                
-                <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
-                     @mouseover="updateProduct(index)" :style="{ backgroundColor:variant.variantColor }">
-                </div>
-                <select>
-                    <option v-for="size in sizes">{{size}}</option>
-                </select>
-                <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Добавить в корзину</button>
-                <button v-on:click="removeFromCart">Удалить из корзины</button>
-                <br>
-                <a v-bind:href="link">Похожее</a>
-            </div>
-            <div>
-            <h2>Reviews</h2>
-            <p v-if="!reviews.length">There are no reviews yet.</p>
-            <ul>
-              <li v-for="review in reviews">
-              <p>{{ review.name }}</p>
-              <p>Rating: {{ review.rating }}</p>
-              <p>{{ review.review }}</p>
-              </li>
-            </ul>
-           </div> <product-review @review-submitted="addReview"></product-review>
-       </div>`,
-    data() {
-        return {
-            product: "Носки",
-            brand: 'Vue Mastery',
-            description: "Пара теплых, пушистых носков",
-            selectedVariant: 0,
-            altText: 'Socks',
-            link: "https://www.ozon.ru/category/odezhda-obuv-i-aksessuary-7500/?text=%D0%BD%D0%BE%D1%81%D0%BA%D0%B8&clid=11468697-1",
-            onSale: [
-                {
-                    not: "Нет распродажи",
-                    yes: "Распродажа"
-                }
-            ],
-            // details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
-            variants: [
-                {
-                    variantId: 2234,
-                    variantColor: 'green',
-                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                    variantQuantity: 10
-                },
-                {
-                    variantId: 2235,
-                    variantColor: 'blue',
-                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                    variantQuantity: 0
-                },
-            ],
-            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-            cart: [],
-            reviews: []
-        }
-    },
-    props: {
-        premium: {
-            type: Boolean,
-            required: false
-        }
-    },
-    methods: {
-        addToCart() {
-            this.$emit('remove-from-cart',
-                this.variants[this.selectedVariant].variantId);
-        },
-        removeFromCart(index) {
-            this.$emit('remove-from-cart',
-            this.variants[this.selectedVariant].variantId);
-
-        },
-        updateProduct(index) {
-            this.selectedVariant = index;
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].variantQuantity
-        },
-        sale(){
-            return this.onSale.not  + ' ' + this.brand + ' ' + this.product;
-        },
-        shipping() {
-            if (this.premium) {
-                return "Бесплатно";
-            } else {
-                return "50₽"
-            }
-        }
-    },
-})
 Vue.component('product-review', {
     template: `
        <form class="review-form" @submit.prevent="onSubmit">
@@ -195,6 +82,132 @@ Vue.component('product-details', {
     }}
 
 })
+
+Vue.component('product', {
+    props: {
+    premium: {
+        type: Boolean,
+        required: true
+    }
+},
+template: `
+<div class="product">
+<div class="product-image">
+       <img :src="image" :alt="altText"/>
+   </div>
+   <div class="product-info">
+       <h1>{{ title }}</h1>
+       <p>{{description}}</p>
+       <span class="sale">{{ sale }}</span>
+       <p v-if="inStock" >В наличии</p>
+       <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
+       <p>Доставка: {{ shipping }}</p>
+       <ul>
+           <li v-for="detail in details">{{ detail }}</li>
+       </ul>
+       <div
+               class="color-box"
+               v-for="(variant, index) in variants"
+               :key="variant.variantId"
+               :style="{ backgroundColor:variant.variantColor }"
+               @mouseover="updateProduct(index)"
+       ></div>
+       <select>
+            <option v-for="size in sizes">{{size}}</option>
+        </select>
+       <button
+               v-on:click="addToCart"
+               :disabled="!inStock"
+               :class="{ disabledButton: !inStock }"
+       >
+           Добавить в корзину
+       </button>
+       <button
+               v-on:click="removeFromCart"
+       >
+           Удалить из корзины
+       </button>
+       <a v-bind:href="link">Похожее</a>    
+   </div>           
+   <div>
+        <h2>Reviews</h2>
+        <p v-if="!reviews.length">Отзывов нет.</p>
+        <ul>
+          <li v-for="review in reviews">
+          <p>{{ review.name }}</p>
+          <p>Рейтинг: {{ review.rating }}</p>
+          <p>{{ review.review }}</p>
+          </li>
+        </ul>
+       </div> <product-review @review-submitted="addReview"></product-review>
+   </div>
+`,
+data() {
+    return {
+        product: "Носки",
+        brand: 'Vue Mastery',
+        description: "Пара теплых, пушистых носков",
+        selectedVariant: 0,
+        altText: "Socks",
+        link: "https://www.ozon.ru/category/odezhda-obuv-i-aksessuary-7500/?text=%D0%BD%D0%BE%D1%81%D0%BA%D0%B8&clid=11468697-1",
+        onSale: false,
+        details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        reviews: []
+    }
+},
+methods: {
+    addToCart() {
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+    },
+    removeFromCart(index) {
+        this.$emit('remove-from-cart',
+        this.variants[this.selectedVariant].variantId);
+     },
+    updateProduct(index) {
+        this.selectedVariant = index;
+    },
+    addReview(productReview) {
+        this.reviews.push(productReview)
+    }
+},
+computed: {
+    title() {
+        return this.brand + ' ' + this.product;
+    },
+    image() {
+        return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+        return this.variants[this.selectedVariant].variantQuantity
+    },
+    sale(){
+        return this.onSale ? (`${this.brand} ${this.product} Скидка!`) : (`${this.brand} ${this.product} без скидки.`);
+     },
+    shipping() {
+        if (this.premium) {
+            return "Бесплатно";
+        } else {
+            return "50₽"
+        }
+    }
+}
+})
+
     let app = new Vue({
     el: '#app',
     data: {
