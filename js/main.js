54a247115e75fd454149da73d4d7b84eb00da4dc
Кексель Наина Сергеевна <gr433_kense@ttit.local>
2025-02-13 11:13:33 +0700

    last

--- js/main.js
+++ js/main.js
@@ -1,47 +1,46 @@
+
+
 let eventBus = new Vue()
+
 Vue.component('product-review', {
     template: `
-       <form class="review-form" @submit.prevent="onSubmit">
-       <p v-if="errors.length">
-             <b>Please correct the following error(s):</b>
-             <ul>
-               <li v-for="error in errors">{{ error }}</li>
-             </ul>
-       </p>
-         <p>
-           <label for="name">Имя:</label>
-           <input id="name" v-model="name" placeholder="name">
-         </p>
-        
-         <p>
-           <label for="review"Отзыв:</label>
-           <textarea id="review" v-model="review"></textarea>
-         </p>
-         <p>
-           <label for="rating">Рейтинг:</label>
-           <select id="rating" v-model.number="rating">
-             <option>5</option>
-             <option>4</option>
-             <option>3</option>
-             <option>2</option>
-             <option>1</option>
-           </select>
-         </p>
-         <p>
-           <label class="rec">Смогли бы вы порекоментовать этот товар?</label>
-           <input type="radio" id="rec" name="drone" value="Да" v-model="recommended">
-           <label for="rec">Yes</label>
-           <input type="radio" id="not_rec" name="drone" value="Нет" v-model="recommended">
-           <label for="not_rec">Yesn't</label>
-         </p>
-         <p>
-           <input type="submit" value="Submit"> 
-         </p>
-    
+        <form class="review-form" @submit.prevent="onSubmit">
+            <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                    <li v-for="error in errors">{{ error }}</li>
+                </ul>
+            </p>
+            <p>
+                <label for="name">Имя:</label>
+                <input id="name" v-model="name" placeholder="name">
+            </p>
+            <p>
+                <label for="review">Отзыв:</label>
+                <textarea id="review" v-model="review"></textarea>
+            </p>
+            <p>
+                <label for="rating">Рейтинг:</label>
+                <select id="rating" v-model.number="rating">
+                    <option>5</option>
+                    <option>4</option>
+                    <option>3</option>
+                    <option>2</option>
+                    <option>1</option>
+                </select>
+            </p>
+            <p>
+                <label class="rec">Смогли бы вы порекомендовать этот товар?</label>
+                <input type="radio" id="rec" name="drone" value="Да" v-model="recommended">
+                <label for="rec">Yes</label>
+                <input type="radio" id="not_rec" name="drone" value="Нет" v-model="recommended">
+                <label for="not_rec">No</label>
+            </p>
+            <p>
+                <input type="submit" value="Submit">
+            </p>
         </form>
-        
-        
- `,
+    `,
     data() {
         return {
             name: null,
@@ -51,9 +50,9 @@ Vue.component('product-review', {
             recommended: null,
         }
     },
-    methods:{
+    methods: {
         onSubmit() {
-            if(this.name && this.review && this.rating) {
+            if (this.name && this.review && this.rating) {
                 let productReview = {
                     name: this.name,
                     review: this.review,
@@ -63,10 +62,12 @@ Vue.component('product-review', {
                 this.name = null
                 this.review = null
                 this.rating = null
+                this.$emit('close-modal')
             } else {
-                if(!this.name) this.errors.push("Name required.")
-                if(!this.review) this.errors.push("Review required.")
-                if(!this.rating) this.errors.push("Rating required.")
+                this.errors = []
+                if (!this.name) this.errors.push("Name required.")
+                if (!this.review) this.errors.push("Review required.")
+                if (!this.rating) this.errors.push("Rating required.")
             }
         },
     }
@@ -74,168 +75,170 @@ Vue.component('product-review', {
 
 Vue.component('product-details', {
     template: `
-    <ul>
-        <li v-for="detail in details">{{ detail }}</li>
-    </ul>`,
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>`,
     data() {
         return {
             details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
-    }}
-
-})
-
-Vue.component('product', {
-    props: {
-    premium: {
-        type: Boolean,
-        required: true
-    }
-},
-template: `
-<div class="product">
-<div class="product-image">
-       <img :src="image" :alt="altText"/>
-   </div>
-   <div class="product-info">
-       <h1>{{ title }}</h1>
-       <p>{{description}}</p>
-       <span class="sale">{{ sale }}</span>
-       <p v-if="inStock" >В наличии</p>
-       <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
-       <p>Доставка: {{ shipping }}</p>
-       <ul>
-           <li v-for="detail in details">{{ detail }}</li>
-       </ul>
-       <div
-               class="color-box"
-               v-for="(variant, index) in variants"
-               :key="variant.variantId"
-               :style="{ backgroundColor:variant.variantColor }"
-               @mouseover="updateProduct(index)"
-       ></div>
-       <select>
-            <option v-for="size in sizes">{{size}}</option>
-        </select>
-       <button
-               v-on:click="addToCart"
-               :disabled="!inStock"
-               :class="{ disabledButton: !inStock }"
-       >
-           Добавить в корзину
-       </button>
-       <button
-               v-on:click="removeFromCart"
-       >
-           Удалить из корзины
-       </button>
-       <a v-bind:href="link">Похожее</a>    
-   </div>           
-   <div>
-       <product-tabs :reviews="reviews"></product-tabs>
-   </div>
-`,
-data() {
-    return {
-        product: "Носки",
-        brand: 'Vue Mastery',
-        description: "Пара теплых, пушистых носков",
-        selectedVariant: 0,
-        altText: "Socks",
-        link: "https://www.ozon.ru/category/odezhda-obuv-i-aksessuary-7500/?text=%D0%BD%D0%BE%D1%81%D0%BA%D0%B8&clid=11468697-1",
-        onSale: false,
-        details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0
-            }
-        ],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
-        reviews: []
-    }
-},
-mounted() {
-    eventBus.$on('review-submitted', productReview => {
-        this.reviews.push(productReview)
-    })
-},
-methods: {
-    addToCart() {
-        this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
-    },
-    removeFromCart(index) {
-        this.$emit('remove-from-cart',
-        this.variants[this.selectedVariant].variantId);
-     },
-    updateProduct(index) {
-        this.selectedVariant = index;
-    },
-    addReview(productReview) {
-        this.reviews.push(productReview)
-    }
-},
-computed: {
-    title() {
-        return this.brand + ' ' + this.product;
-    },
-    image() {
-        return this.variants[this.selectedVariant].variantImage;
-    },
-    inStock() {
-        return this.variants[this.selectedVariant].variantQuantity
-    },
-    sale(){
-        return this.onSale ? (`${this.brand} ${this.product} Скидка!`) : (`${this.brand} ${this.product} без скидки.`);
-     },
-    shipping() {
-        if (this.premium) {
-            return "Бесплатно";
-        } else {
-            return "50₽"
         }
     }
-}
+})
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true
+        }
+    },
+    template: `
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" :alt="altText"/>
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p>{{description}}</p>
+                <span class="sale">{{ sale }}</span>
+                <p v-if="inStock">В наличии</p>
+                <p v-else :class="{ OutOfStock:!inStock }">Нет в наличии</p>
+                <p>Доставка: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+                <div
+                    class="color-box"
+                    v-for="(variant, index) in variants"
+                    :key="variant.variantId"
+                    :style="{ backgroundColor:variant.variantColor }"
+                    @mouseover="updateProduct(index)"
+                ></div>
+                <select>
+                    <option v-for="size in sizes">{{size}}</option>
+                </select>
+                <button
+                    v-on:click="addToCart"
+                    :disabled="!inStock"
+                    :class="{ disabledButton: !inStock }"
+                >
+                    Добавить в корзину
+                </button>
+                <button
+                    v-on:click="removeFromCart"
+                >
+                    Удалить из корзины
+                </button>
+                <a v-bind:href="link">Похожее</a>
+            </div>
+            <div>
+                <product-tabs :reviews="reviews"></product-tabs>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            product: "Носки",
+            brand: 'Vue Mastery',
+            description: "Пара теплых, пушистых носков",
+            selectedVariant: 0,
+            altText: "Socks",
+            link: "https://www.ozon.ru/category/odezhda-obuv-i-aksessuary-7500/?text=%D0%BD%D0%BE%D1%81%D0%BA%D0%B8&clid=11468697-1",
+            onSale: false,
+            details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0
+                }
+            ],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+            reviews: []
+        }
+    },
+    mounted() {
+        eventBus.$on('review-submitted', productReview => {
+            this.reviews.push(productReview)
+        })
+    },
+    methods: {
+        addToCart() {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+        },
+        removeFromCart(index) {
+            this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
+        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+        }
+    },
+    computed: {
+        title() {
+            
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            return this.onSale ? `${this.brand} ${this.product} Скидка!` : `${this.brand} ${this.product} без скидки.`;
+        },
+        shipping() {
+            if (this.premium) {
+                return "Бесплатно";
+            } else {
+                return "50₽"
+            }
+        }
+    }
 })
 
 Vue.component('product-tabs', {
     template: `
-     <div>   
-       <ul>
-         <span class="tab"
-               :class="{ activeTab: selectedTab === tab }"
-               v-for="(tab, index) in tabs"
-               @click="selectedTab = tab"
-         >{{ tab }}</span>
-       </ul>
-       <div v-show="selectedTab === 'Отзывы'">
-         <p v-if="!reviews.length">Нет отзывов.</p>
-         <ul>
-           <li v-for="review in reviews">
-           <p>{{ review.name }}</p>
-           <p>Rating: {{ review.rating }}</p>
-           <p>{{ review.review }}</p>
-           </li>
-         </ul>
-       </div> 
-       <div v-show="selectedTab === 'Оставить отзыв'">
-         <product-review></product-review>
-     </div>
-     <div v-show="selectedTab === 'Доставка'">
-        <product-del></product-del>
-    </div>
-    <div v-show="selectedTab === 'Детали'">
-        <product-detail></product-detail>
-    </div>
-     </div>
-`,
+        <div>
+            <ul>
+                <span class="tab"
+                      :class="{ activeTab: selectedTab === tab }"
+                      v-for="(tab, index) in tabs"
+                      @click="selectTab(tab)"
+                >{{ tab }}</span>
+            </ul>
+            <div v-show="selectedTab === 'Отзывы'">
+                <p v-if="!reviews.length">Нет отзывов.</p>
+                <ul>
+                    <li v-for="review in reviews">
+                        <p>{{ review.name }}</p>
+                        <p>Rating: {{ review.rating }}</p>
+                        <p>{{ review.review }}</p>
+                    </li>
+                </ul>
+            </div>
+            <div v-show="selectedTab === 'Оставить отзыв'">
+                <div class="modal" :class="{ open: isModalOpen }">
+                    <div class="modal-content">
+                        <span class="close-button" @click="closeModal">&times;</span>
+                        <product-review @close-modal="closeModal"></product-review>
+                    </div>
+                </div>
+            </div>
+            <div v-show="selectedTab === 'Доставка'">
+                <product-del></product-del>
+            </div>
+            <div v-show="selectedTab === 'Детали'">
+                <product-detail></product-detail>
+            </div>
+        </div>
+    `,
     props: {
         reviews: {
             type: Array,
@@ -245,17 +248,29 @@ Vue.component('product-tabs', {
     data() {
         return {
             tabs: ['Отзывы', 'Оставить отзыв', 'Доставка', 'Детали'],
-            selectedTab: 'Отзывы'
+            selectedTab: 'Отзывы',
+            isModalOpen: false
+        }
+    },
+    methods: {
+        selectTab(tab) {
+            this.selectedTab = tab;
+            if (tab === 'Оставить отзыв') {
+                this.isModalOpen = true;
+            }
+        },
+        closeModal() {
+            this.isModalOpen = false;
+            this.selectedTab = 'Отзывы'; // Возвращаемся на вкладку "Отзывы"
         }
     }
 })
-
 Vue.component('product-detail', {
-    template:`
-    <ul>
-        <li v-for="detail in details">{{ detail }}</li>
-    </ul>`,
-    data () {
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>`,
+    data() {
         return {
             details: ['80% хлопок', '20% полиэстер', 'Унисекс'],
         }
@@ -263,10 +278,8 @@ Vue.component('product-detail', {
 })
 
 Vue.component('product-del', {
-    template:`
-    <p>Доставка: {{ shipping }}</p>`,
-    data () {
-    },
+    template: `
+        <p>Доставка: {{ shipping }}</p>`,
     computed: {
         shipping() {
             if (this.premium) {
@@ -278,7 +291,7 @@ Vue.component('product-del', {
     }
 })
 
-    let app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
         premium: false,
@@ -296,5 +309,3 @@ Vue.component('product-del', {
         }
     },
 })
-
-
